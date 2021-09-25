9f46922ca3126caa34851922ff3feb03b00fea6f
moezkh2 <khorchani_moez_eddine@outlook.com>
2021-09-25 17:08:02 +0100

    heroku proxy3

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const path = require('path')
 const mongoose = require('mongoose')
 require('dotenv').config()
 mongoose.connect(process.env.MONGO_URI).then(console.log("database connected")).catch((err) => { console.log(err) })
@@ -9,7 +10,7 @@ app.use("/service", require('./route/RouteService'))
 if (process.env.NODE_ENV === "production") {
     app.use(express.static('./client/build'))
     app.get('*', (req, res) => {
-        res.sendFile(resolve(process.cwd(), 'client/build/index.html'))
+        res.sendFile(path.resolve(__dirname, 'client/build/index.html'));
     })
 }
 app.listen(process.env.PORT, (err) => {
