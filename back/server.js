8f68cae94ac10ef32ca9e40aebe5fbca9e6d3e8b
moezkh2 <khorchani_moez_eddine@outlook.com>
2021-09-25 12:22:56 +0100

    heroko

--- back/server.js
+++ back/server.js
@@ -1,11 +1,15 @@
-const express= require('express')
-const mongoose= require('mongoose')
+const express = require('express')
+const mongoose = require('mongoose')
 require('dotenv').config()
-mongoose.connect(process.env.MONGO_URI).then(console.log("database connected")).catch((err)=>{console.log(err)})
+mongoose.connect(process.env.MONGO_URI).then(console.log("database connected")).catch((err) => { console.log(err) })
 const app = express()
 app.use(express.json())
-app.use("/user",require('./route/RouteUser'))
-app.use("/service",require('./route/RouteService'))
-app.listen(process.env.PORT,(err)=>{
-    err?console.log(err):console.log("server is running")
+app.use("/user", require('./route/RouteUser'))
+app.use("/service", require('./route/RouteService'))
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static('../build'))
+
+}
+app.listen(process.env.PORT, (err) => {
+    err ? console.log(err) : console.log("server is running")
 })
