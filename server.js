dc901996c9826e1a6ef675677fae47a8e656583c
moezkh2 <khorchani_moez_eddine@outlook.com>
2021-09-27 16:09:47 +0100

    fix 4

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.use("/user", require('./route/RouteUser'))
 app.use("/service", require('./route/RouteService'))
 if (process.env.NODE_ENV === "production") {
     app.use(express.static('./client/build'))
-    app.get('*', (req, res) => {
+    app.get('/*', (req, res) => {
         res.sendFile(path.resolve(__dirname, 'client/build/index.html'));
     })
 }
